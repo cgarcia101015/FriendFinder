f728011f9616b883517e5b3bd4e6325133a90edb
Carlos M Garcia <cgarcia101015@gmail.com>
2019-01-02 23:03:23 -0500

    added friends sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,8 +3,8 @@
 
 module.exports = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Riz Ahmed",
+    "photo":"https://c.tribune.com.pk/2017/10/1527221-rizahmed-1507614182-285-640x480.png",
     "scores":[
         5,
         1,
@@ -17,5 +17,70 @@ module.exports = [
         4,
         1
       ]
+},
+{
+    "name":"Will Smith",
+    "photo":"https://d2au9l3ykcrs3m.cloudfront.net/5670/public/public/system/posters/81423/medium/Will_Smith_1519944859.jpg",
+    "scores":[
+        1,
+        4,
+        2,
+        2,
+        5,
+        2,
+        1,
+        1,
+        3,
+        3
+      ]
+},
+{
+    "name":"Ted Danson",
+    "photo":"https://bento.cdn.pbs.org/hostedbento-prod/filer_public_thumbnails/filer_public/findingyourroots/Danson_Ted_Headshot%20HIGH%20RES.jpg__320x400_q85_subsampling-2.jpg",
+    "scores":[
+        4,
+        3,
+        5,
+        5,
+        1,
+        1,
+        3,
+        2,
+        5,
+        5
+      ]
+},
+{
+    "name":"Homer Simpson",
+    "photo":"https://timeentertainment.files.wordpress.com/2013/05/fictioninfluence_poll_homersimpson.jpg?w=260",
+    "scores":[
+        4,
+        2,
+        3,
+        5,
+        1,
+        5,
+        3,
+        4,
+        1,
+        4
+      ]
+},
+{
+    "name":"Martha Stewart",
+    "photo":"http://www.cottages-gardens.com/Over-the-Wire/May-2017/Martha-Stewart-2017-HCG-IDA-Innovator/",
+    "scores":[
+        4,
+        2,
+        4,
+        5,
+        1,
+        2,
+        5,
+        1,
+        1,
+        2
+      ]
 }
+
 ]
