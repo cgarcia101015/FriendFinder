4b7e4d7da984010473221bb9d6d9b48ec0c96024
Carlos M Garcia <cgarcia101015@gmail.com>
2019-01-05 16:00:36 -0500

    got comparison logic working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,29 @@ module.exports = function (app) {
     res.json(friendsData);
   });
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
-  });
+ app.post("/api/friends", function(req, res) {
+  var userInput = req.body;
+  var userScores = userInput.scores;
+
+  console.log(req.body);
+   var totalDifference = 0;
+   var matchingFriend;
+
+for (var j = 0; j < friendsData.length; j++) {
+  var currentFriendScore = friendsData[j].scores;
+  var currentDifference = 0;
+  for (var i = 0; i < currentFriendScore.length; i++) {
+    currentDifference += Math.abs(userScores[i] - currentFriendScore[i]);
+  
+  }
+  if (totalDifference === 0 || currentDifference < totalDifference) {
+    totalDifference = currentDifference;
+    matchingFriend = friendsData[j];
+  }
 }
+  friendsData.push(userInput);
+
+  res.json(matchingFriend);
+ })
+}
+
