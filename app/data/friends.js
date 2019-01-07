55cadceab4d24cfb1fe2d10c4151572f825f9f99
Carlos M Garcia <cgarcia101015@gmail.com>
2019-01-06 23:20:07 -0500

    missing modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,80 +6,80 @@ module.exports = [
     "name":"Riz Ahmed",
     "photo":"https://c.tribune.com.pk/2017/10/1527221-rizahmed-1507614182-285-640x480.png",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
       ]
 },
 {
     "name":"Will Smith",
     "photo":"https://d2au9l3ykcrs3m.cloudfront.net/5670/public/public/system/posters/81423/medium/Will_Smith_1519944859.jpg",
     "scores":[
-        1,
-        4,
-        2,
-        2,
-        5,
-        2,
-        1,
-        1,
-        3,
-        3
+        "1",
+        "4",
+        "2",
+        "2",
+        "5",
+        "2",
+        "1",
+        "1",
+        "3",
+        "3"
       ]
 },
 {
     "name":"Ted Danson",
     "photo":"https://bento.cdn.pbs.org/hostedbento-prod/filer_public_thumbnails/filer_public/findingyourroots/Danson_Ted_Headshot%20HIGH%20RES.jpg__320x400_q85_subsampling-2.jpg",
     "scores":[
-        4,
-        3,
-        5,
-        5,
-        1,
-        1,
-        3,
-        2,
-        5,
-        5
+        "4",
+        "3",
+        "5",
+        "5",
+        "1",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
       ]
 },
 {
     "name":"Homer Simpson",
     "photo":"https://timeentertainment.files.wordpress.com/2013/05/fictioninfluence_poll_homersimpson.jpg?w=260",
     "scores":[
-        4,
-        2,
-        3,
-        5,
-        1,
-        5,
-        3,
-        4,
-        1,
-        4
+        "4",
+        "2",
+        "3",
+        "5",
+        "1",
+        "5",
+        "3",
+        "4",
+        "1",
+        "4"
       ]
 },
 {
     "name":"Martha Stewart",
     "photo":"http://www.cottages-gardens.com/Over-the-Wire/May-2017/Martha-Stewart-2017-HCG-IDA-Innovator/",
     "scores":[
-        4,
-        2,
-        4,
-        5,
-        1,
-        2,
-        5,
-        1,
-        1,
-        2
+        "4",
+        "2",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "1",
+        "1",
+        "2"
       ]
 }
 
