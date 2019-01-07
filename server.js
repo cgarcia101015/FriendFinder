a6bfa9498fd958d763dfbbfabaa36ee0770829f4
Carlos M Garcia <cgarcia101015@gmail.com>
2019-01-07 00:01:28 -0500

    missing modal part

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
+var PORT = process.env.PORT || 3000;
 var app = express();
-var PORT = 3000;
+// var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
